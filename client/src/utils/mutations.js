494c4daffbb479604ffc3a4d442565cabdd1d58c
Bondit Sinlee <bonditsinlee@gmail.com>
2023-02-16 21:35:11 -0600

    using apollo client to define four graphql mutation operations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,11 +25,11 @@ export const ADD_USER = gql`
 `;
 
 export const ADD_POST= gql`
-  mutation addThought($thoughtText: String!) {
-    addThought(thoughtText: $thoughtText) {
+  mutation addPost($postText: String!) {
+    addPost(postText: $postText) {
       _id
-      thoughtText
-      thoughtAuthor
+      postText
+      postAuthor
       createdAt
       comments {
         _id
@@ -40,11 +40,11 @@ export const ADD_POST= gql`
 `;
 
 export const ADD_COMMENT = gql`
-  mutation addComment($thoughtId: ID!, $commentText: String!) {
-    addComment(thoughtId: $thoughtId, commentText: $commentText) {
+  mutation addComment($posttId: ID!, $commentText: String!) {
+    addComment(posttId: $postId, commentText: $commentText) {
       _id
-      thoughtText
-      thoughtAuthor
+      postText
+      postAuthor
       createdAt
       comments {
         _id
