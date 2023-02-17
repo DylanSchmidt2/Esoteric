5d2d3c7f56343ec361de26f5f626c830c9ec2e70
Bondit Sinlee <bonditsinlee@gmail.com>
2023-02-17 02:11:07 -0600

    defines four gql queries that contains different fields and arguments

---
+++ client/src/utils/queries.js
@@ -0,0 +1,62 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+    query user($username: String!){
+        user(username: $username) {
+            _id
+            username
+            email
+            posts {
+                _id
+                postText
+                createdAt
+            }
+        }
+    }
+`;
+
+export const QUERY_POSTS = gql`
+    query getThoughts {
+        thougths {
+            _id
+            postText
+            postAuthor
+            createdAt
+        }
+    }
+`
+
+export const QUERY_SINGLE_POST = gql`
+    query getSinglePost($postId: ID!) {
+        post(id: $postId) {
+            _id
+            postText
+            postAuthor
+            createdAt
+            comments {
+                _id
+                commentText
+                commentAuthor
+                createdAt
+            }
+        }
+    }
+
+`;
+
+export const QUERY_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            posts {
+                _id
+                postText
+                postAuthor
+                createdAt    
+            }
+        }
+    }
+    
+`
