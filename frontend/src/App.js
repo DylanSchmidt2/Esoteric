6ad7088fa9dbc32f1a7d8230258f1fdba01f536c
Bondit Sinlee <bonditsinlee@gmail.com>
2023-02-22 19:54:28 -0600

    React props, Apollo context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,74 +1,74 @@
-HomepageTailwind
+import React from 'react';
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import {Routes, Route } from 'react-router-dom'
+import Header from  './components/Header/Header'
+import Home from './pages/Post'
+import Login from  './pages/Login'
+import Register from './pages/Register'
+import Landing from './pages/Landing';
+
+
+const fakeUser = true;
+
+// Construct main gql API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+//Construct request middleware that will attach the JWT token to every request header.
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token form local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
+const client = new ApolloClient({
+  // set up client to execute the 'authLink
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  });
+
 function App() {
   return (
-    <html>
-  <head>
-    <meta charset="utf-8" />
-    <title>Landing gradients</title>
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-    />
-    <link rel="stylesheet" href="./static/output.css" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,300;0,400;0,500;0,600;1,400&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600;700&display=swap"
-      rel="stylesheet"
-    />
-  </head>
-
-  <body className="h-screen bg-gradient-to-br from-slate-600 to-slate-900">
-    <div
-      className="text-gray-300 container mx-auto p-8 overflow-hidden md:rounded-lg md:p-10 lg:p-12"
-    >
-      <div className="flex space-x-12 space-y-2">
-        <h1 className="font-Raleway text-5xl font-Large">ESOTERIC</h1>
-        <a
-          href=""
-          className="self-start  px-3 py-2 leading-none text-white-200 border border-white-800 rounded-lg focus:outline-none focus:shadow-outline bg-gradient-to-b hover:from-indigo-500 from-gray-900 to-black"
-        >
-          SignUp
-        </a>
-        <a
-          href=""
-          className="self-start ml-0 px-4 py-2 leading-none text-white-200 border border-white-800 rounded-lg focus:outline-none focus:shadow-outline bg-gradient-to-b hover:from-indigo-500 from-gray-900 to-black"
-        >
-          Login
-        </a>
-      </div>
-
-      <div className="h-32 md:h-40"></div>
-
-      <p
-        className="font-sans text-3xl font-bold text-gray-200 max-w-3xl lg:text-6xl lg:pr-24 md:text-6xl"
-      >
-        Connect with like minded individuals to share and exploare thoughts about coding
-      </p>
-      
+   <ApolloProvider client={client}>
     
-</div>
-     
-  </body>
-</html>
-
-import './App.css';
-
-import SignupForm from './components/SignupForm';
-import LoginForm from './components/LoginForm';
-
-function App() {
-  return (
-    //<SignupForm/>
-    //<LoginForm/>
-    // Must require in div
-    <div className="App"> 
-      
-
-    </div>
- main
+      <Header 
+      props={ fakeUser }/>
+        <Routes>
+          <Route
+            path="/"
+            element ={<Landing />}
+          />
+          <Route
+          path="/Home"
+          element={<Home />}
+          />
+          <Route
+            path="/login"
+            element ={<Login />}
+          />
+          <Route
+            path="/register"
+            element ={<Register />}
+          />
+       </Routes>
+    
+   </ApolloProvider>
   );
 }
+
 export default App;
